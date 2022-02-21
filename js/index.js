f3fef6860d42f90db24007df26feb05fd0db3545
Miguel <migurlacade99@gmail.com>
2022-02-20 19:41:34 -0500

    Se corrigió el footer, el carrucel y se optó por usar grid area para organizar los elementos, se intentó agregar un dropdown menu en servicios

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
 const navToggle = document.querySelector(".nav-toggle")
-const navUl = document.querySelector(".navbar-ul")
+const navUl = document.querySelector(".navigationbar-ul")
 
 navToggle.addEventListener("click", ()=> {
-    navUl.classList.toggle("navbar-ul_visible");
+    navUl.classList.toggle("navigationbar-ul_visible");
 
     if (navMenu.classList.contains("nav-menu_visible")){
         navToggle.setAttribute("aria-label","Cerrar menú");
     } else {
         navToggle.setAttribute("aria-label","Abrir menú")
     }
+});
+
+const dropdownToggleFinanciamiento = document.querySelector(".dropdown-toggle-financiamiento");
+const dropdownFinanciamientoContent = document.querySelector(".dropdown-financiamiento-content");
+const dropdownToggleTiendas = document.querySelector(".dropdown-toggle-tiendas");
+const dropdownTiendasContent = document.querySelector(".dropdown-tiendas-content");
+
+dropdownToggleFinanciamiento.addEventListener("click", ()=> {
+    dropdownFinanciamientoContent.classList.toggle("dropdown-financiamiento-content_visible");
+});
+
+
+
+dropdownToggleTiendas.addEventListener("click", ()=> {
+    dropdownTiendasContent.classList.toggle("dropdown-tiendas-content_visible");
 });
